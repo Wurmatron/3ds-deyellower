6383f153e2f18e177b703f2183b2d7c77c68bc43
Wurmatron <wurmatron@wurmcraft.com>
2024-10-10 19:49:26 -0400

    Added Sleep

--- source/main.c
+++ source/main.c
@@ -4,6 +4,7 @@
 #include <3ds.h>
 
 PrintConsole topScreen, bottomScreen;
+long timeUntilSleep = 100000;
 
 void displayMenu() {
 	consoleSelect(&bottomScreen);
@@ -12,9 +13,11 @@ void displayMenu() {
    	printf("\x1b[13;5HSettings");
     printf("\x1b[13;30HBoth");
     printf("\x1b[8;18HTop");
-    printf("\x1b[18;16HBotttom");
+    printf("\x1b[20;16HBotttom");
 	printf("\x1b[28;10HPress Start to stop.");
 	printf("\x1b[30;13Hby Wurmatron v0");
+	consoleSelect(&topScreen);
+	printf("\x1b[30;38H%ld", timeUntilSleep);
 }
 
 void clearScreen() {
@@ -30,10 +33,10 @@ int main(int argc, char **argv)
 	consoleInit(GFX_TOP, &topScreen);
 	consoleInit(GFX_BOTTOM, &bottomScreen);
 
-    displayMenu();
 	bool top = false;
 	bool bottom = false;
-	bool menu = false;
+	bool menu = true;
+	long untilWake = 10000;
 
 	// Main loop
 	while (aptMainLoop())
@@ -42,9 +45,11 @@ int main(int argc, char **argv)
 		hidScanInput();
 		u32 kDown = hidKeysDown();
 
+		// Check for key input
 		if (kDown & KEY_DUP) {
 			top = true;
 			bottom = false;
+			menu = false;
 			clearScreen();
 		} else if(kDown & KEY_DRIGHT) {
 			top = true;
@@ -63,24 +68,43 @@ int main(int argc, char **argv)
 			clearScreen();
 		}
 
-		// Set top to white
-		if (top) {
-			consoleSelect(&topScreen);
-			for (int i = 0; i < 1500;i++){
-            	printf("\x1b[47;30m \x1b[0m");
-            }
-		}
-
-		// Set bottom to white
-		if (bottom) {
-			consoleSelect(&bottomScreen);
-            for (int i = 0; i < 1200;i++){
-            	printf("\x1b[47;30m \x1b[0m");
-            }
-		}
-
 		if(menu) {
 			displayMenu();
+		} else {
+			if(timeUntilSleep > 0) {
+				timeUntilSleep--;
+				// Set top to white
+				if (top) {
+					consoleSelect(&topScreen);
+					for (int i = 0; i < 1500;i++){
+        	    		printf("\x1b[47;30m \x1b[0m");
+        	  	  }
+				} else {
+					consoleSelect(&topScreen);
+					consoleClear();
+				}
+				// Set bottom to white
+				if (bottom) {
+					consoleSelect(&bottomScreen);
+         	 	  for (int i = 0; i < 1200;i++){
+         			   	printf("\x1b[47;30m \x1b[0m");
+         		   }
+				} else {
+					consoleSelect(&bottomScreen);
+					consoleClear();
+				}
+			} else {
+				clearScreen();
+				consoleSelect(&bottomScreen);
+				printf("\x1b[16;14HSleeping ...");
+				printf("\x1b[20;12HSleep Left: %ld", untilWake);
+				if(untilWake > 0) {
+					untilWake--;
+				} else {
+					timeUntilSleep = 100000;
+					untilWake = 10000;
+				}	
+			} 
 		}
 
 		// Close When Start is pushed
